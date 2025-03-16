7acc18a7c697d4cae7de356b86dd07201e99d7df
JackS <djsiegers42@tntech.edu>
2025-03-16 17:48:59 -0500

    added server code

--- client.py
+++ client.py
@@ -1,1 +1,39 @@
+import socket
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+import base64
+
+# Encryption settings
+KEY = b'ThisIsASecretKey'  # 16 bytes for AES-128
+IV = b'ThisIsAnIVVector'  # 16 bytes IV
+
+def encrypt_message(message):
+    cipher = AES.new(KEY, AES.MODE_CBC, IV)
+    encrypted_bytes = cipher.encrypt(pad(message.encode(), AES.block_size))
+    return base64.b64encode(encrypted_bytes).decode()
+
+def decrypt_message(encrypted_message):
+    cipher = AES.new(KEY, AES.MODE_CBC, IV)
+    decrypted_bytes = unpad(cipher.decrypt(base64.b64decode(encrypted_message)), AES.block_size)
+    return decrypted_bytes.decode()
+
+# TCP Client
+class TCPClient:
+    def __init__(self, server_host='127.0.0.1', server_port=12345):
+        self.server_host = server_host
+        self.server_port = server_port
+    
+    def send_message(self, message):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((self.server_host, self.server_port))
+        encrypted_message = encrypt_message(message)
+        client_socket.send(encrypted_message.encode())
+        response = decrypt_message(client_socket.recv(1024).decode())
+        print(f"Server response: {response}")
+        client_socket.close()
+
+if __name__ == "__main__":
+    message = input("Enter a message: ")
+    client = TCPClient()
+    client.send_message(message)
 
